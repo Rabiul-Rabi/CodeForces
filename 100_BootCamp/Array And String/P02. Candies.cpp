0581dbb1cccf59175879ce21b62feddfb4398cdc
Rabiul-Rabi <66696997+Rabiul-Rabi@users.noreply.github.com>
2022-03-19 03:30:24 +0600

    Create P02. Candies.cpp

---
+++ 100_BootCamp/Array And String/P02. Candies.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+        int n;
+        cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+       cin>>arr[i];
+    }
+    int a,b,sum=0;
+    cin>>a>>b;
+    for (int i = a; i <=b ; ++i) {
+            sum=sum+arr[i];
+    }
+    cout<<sum<<endl;
+}
